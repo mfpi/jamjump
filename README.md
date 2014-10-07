275afff767e98f26433ed56f91336f16554831bb
mf <mf@tt4.at>
2014-10-07 20:37:36 +0200

    Add credits to titlescreen, gameplay description

--- README.md
+++ README.md
@@ -11,9 +11,24 @@ Gamejam Graz 2014 II Entry
 
 ### Programming
  * Matthias Frey
- * Daniel Perz 
+ * Daniel Perz
  * Marco Fruhwirth
 
 ### Music and Sound
  * Daniel Perz
+
+
+## Gameplay
+
+A two player co-op game, with elements of a classic 2d platformer game.
+Both players have to reach a special glowing spot on the map (the 'comet') to
+complete the level. To do so, they have to create blocks out of thin air -
+which unfortunately makes another block somewhere else on the map vanish.
+
+
+# Screenshots
+
+![Title](https://raw.githubusercontent.com/mfit/jamjump/master/press/jamjump_screen1.png)
+
+
 
--- phaser/game/states/menu.js
+++ phaser/game/states/menu.js
@@ -7,23 +7,33 @@ Menu.prototype = {
 
   },
   create: function() {
-    var style = { font: '65px Arial', fill: '#ffffff', align: 'center'};
+    var style = { font: '65px Arial', fill: '#ffffff', align:'center'};
+    var style_small = { font: '16px Arial', fill: '#ffffff'};
     var temp_sprite = this.game.add.sprite(0,0, 'background3');
     this.sprite = this.game.add.sprite(this.game.world.centerX, 138, 'titlecharacter');
     this.sprite.anchor.setTo(0.5, 0.5);
     this.sprite.scale.setTo(5,5);
-
-    this.titleText = this.game.add.text(this.game.world.centerX, 300, 'JamJump', style);
-    this.titleText.anchor.setTo(0.5, 0.5);
-
-    this.instructionsText = this.game.add.text(this.game.world.centerX, 400,
-      'Howto play : both players must reach the glowing pink stone... WASD and Arrows ... \n HIT ENTER TO START!',
-      { font: '16px Arial', fill: '#ffffff', align: 'center'});
-    this.instructionsText.anchor.setTo(0.5, 0.5);
-
     this.sprite.angle = -20;
-
     this.game.add.tween(this.sprite).to({angle: 20}, 1000, Phaser.Easing.Linear.NONE, true, 0, 1000, true);
+
+    this.titleText = this.game.add.text(this.game.world.centerX, 280, 'JamJump', style);
+    this.titleText.anchor.setTo(0.5, 0.5);
+    this.startText = this.game.add.text(this.game.world.centerX, 340, 'HIT ENTER TO START!',
+      { font: '16px Arial', fill: '#ffffff', align: 'center'});
+    this.startText.anchor.setTo(0.5, 0.5);
+
+     this.instructionsText = this.game.add.text(40, 400,
+       'HOWTO: \nBoth players must reach the glowing pink stone' +
+       '\nControls: WASD and Arrows\nPress down mid-air to create block',
+       style_small);
+
+    this.creditsText = this.game.add.text(this.game.world.centerX+40, 400,
+      'CREDITS:\nGraphics: \nDorothea Prem, Daniel Perz\n' +
+      'Programming: \nMatthias Frey, Daniel Perz, Marco Fruhwirth\n' +
+      'Music and Sound: \nDaniel Perz',
+    style_small);
+
+
   },
 
   update: function() {

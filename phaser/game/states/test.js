d87ce2575a6b97a27b258a950b7212daf1087bc7
Marco Fruhwirth <fruhwirth.marco@gmail.com>
2014-10-21 20:01:51 +0200

    load test level

--- phaser/game/states/test.js
+++ phaser/game/states/test.js
@@ -4,14 +4,18 @@ function TestState() {}
 
 TestState.prototype = {
   preload: function () {
+    this.game.load.tilemap('platformer', 'assets/levels/tiled_level.json', null, Phaser.Tilemap.TILED_JSON);
+    this.game.load.image('platformer_tiles', 'assets/spritesheet.png');
 
   },
   create: function () {
-    var map = this.add.tilemap('map'); // Preloaded tilemap
+    var map = this.game.add.tilemap('platformer'); // Preloaded tilemap
+    map.addTilesetImage('spritesheet', 'platformer_tiles'); // Preloaded tileset
+    this.layer = map.createLayer('Tile Layer 1');
+    this.layer.resizeWorld();
 
     console.log(map);
 
-    // map.addTilesetImage('tileset', 0); // Preloaded tileset
     // var layer = map.createLayer('Stones'); // This is the default name of the first layer in Tiled
     // layer.resizeWorld(); // Sets the world size to match the size of this layer.
   },
